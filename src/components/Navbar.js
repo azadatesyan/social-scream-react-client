00a0fdcb58baef64ac65d5ae1424d9cca794bec0
Olivier <olivier.atesyan@ieseg.fr>
2020-08-22 23:32:48 +0200

    add link to homepage in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
         <div>
             <Appbar>
                 <Toolbar className="navbar-container">
+                    <Button color="inherit" component={Link} to="/" >Home</Button>
                     <Button color="inherit" component={Link} to="/login" >Login</Button>
                     <Button color="inherit" component={Link} to="/signup">Signup</Button>
                 </Toolbar>
